fdd43e343d79bc5a08671765f256a9386ae23464
“abhijeetardale” <abhijeet.ardale@yahoo.co.in>
2019-12-15 21:16:05 +0000

    added test to check  mock url

--- test/config/AppConfigSpec.scala
+++ test/config/AppConfigSpec.scala
@@ -3,7 +3,8 @@ package config
 import org.scalatest.MustMatchers
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
-import play.api.{Configuration, Environment}
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.{Application, Configuration, Environment}
 import play.api.test.Injecting
 
 class AppConfigSpec extends PlaySpec with GuiceOneAppPerSuite with MustMatchers with Injecting {
@@ -20,7 +21,17 @@ class AppConfigSpec extends PlaySpec with GuiceOneAppPerSuite with MustMatchers 
       "return the configured base url" in {
 
         appConfig.base mustBe "https://bpdts-test-app.herokuapp.com"
+      }
 
+      "return the mock base url" in {
+
+        val app: Application = new GuiceApplicationBuilder().configure(
+          "locationService.base" -> s"http://localhost:8080").build()
+        val config = app.injector.instanceOf[Configuration]
+        val environment = app.injector.instanceOf[Environment]
+
+        val appConfig = new AppConfig(config, environment)
+        appConfig.base mustBe "http://localhost:8080"
       }
     }
   }

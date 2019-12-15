79c572659b8248fcd6cdac8918bd7ee7b20b7ce4
“abhijeetardale” <abhijeet.ardale@yahoo.co.in>
2019-12-15 22:28:10 +0000

    updated code for mime type

--- app/connector/UserLocationConnector.scala
+++ app/connector/UserLocationConnector.scala
@@ -3,6 +3,7 @@ package connector
 import config.AppConfig
 import javax.inject.Inject
 import play.api.libs.json.JsValue
+import play.api.http.MimeTypes.JSON
 import play.api.libs.ws.{WSClient, WSResponse}
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -14,7 +15,7 @@ class UserLocationConnector@Inject()(wsClient: WSClient, appConfig: AppConfig) {
     
     val serviceUrl = s"${appConfig.base}/users"
 
-    wsClient.url(serviceUrl).get().map{
+    wsClient.url(serviceUrl).addHttpHeaders("Content-Type" -> JSON).get().map{
       _.json
     }
 

82f7d4749c6426ccafd777beb65a85b292b4fde0
“abhijeetardale” <abhijeet.ardale@yahoo.co.in>
2019-12-16 07:45:56 +0000

    haversine formula added to calculate the distance

--- app/service/LocationService.scala
+++ app/service/LocationService.scala
@@ -2,8 +2,37 @@ package service
 
 import models.User
 
+//The haversine formula determines the great-circle distance between two points on a sphere given their
+// longitudes and latitudes. Important in navigation, it is a special case of a more general
+// formula in spherical trigonometry, the law of haversines, that relates the sides and angles of
+// spherical triangles.
+//R = earth’s radius
+//Δlat = lat2− lat1; Δlong = long2− long1
+//a = sin²(Δlat/2) + cos(lat1) * cos(lat2) * sin²(Δlong/2)
+//c = 2*atan2(√a, √(1−a)); d = R*c
+
 class LocationService() {
 
-  def locate(user: User) :Boolean = ???
+  private val londonLatitude: Double = 51.50853
+  private val londonLongitude: Double = -0.12574
+  private val EarthRadiusInMiles: Double = 3959.87433
+
+  def locate(user: User): Boolean = {
+
+    val latitudeDistance = Math.toRadians(user.latitude - londonLatitude)
+    val longitudeDistance = Math.toRadians(user.longitude - londonLongitude)
+
+    val sinLat = Math.sin(latitudeDistance / 2)
+    val sinLng = Math.sin(longitudeDistance / 2)
+
+    val a = sinLat * sinLat +
+      (Math.cos(Math.toRadians(user.latitude))
+        * Math.cos(Math.toRadians(londonLatitude))
+        * sinLng * sinLng)
+
+    val c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+
+    (EarthRadiusInMiles * c).round <= 50
+  }
 
 }
--- test/service/LocationServiceSpec.scala
+++ test/service/LocationServiceSpec.scala
@@ -9,13 +9,14 @@ import play.api.test.Injecting
 class LocationServiceSpec extends PlaySpec with GuiceOneAppPerSuite with MustMatchers with Injecting {
 
   val service = new LocationService()
-  val user = User(1, "Maurise", "Shieldon", "mshieldon0@squidoo.com", "192.57.232.111", 34.003135, -117.7228641)
+  val userLondon = User(1, "Maurise", "Shieldon", "mshieldon0@squidoo.com", "192.57.232.111", 51.50853, -0.12574)
+
   "LocationService" when{
 
     "called with user coordinate" must{
 
-     "return true if distance is within 50 miles" in {
-       service.locate(user) mustBe true
+     "return true if distance is 0 mile away from London" in {
+       service.locate(userLondon) mustBe true
      }
 
     }

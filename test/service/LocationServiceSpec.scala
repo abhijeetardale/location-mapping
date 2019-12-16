ee54798a8bea0c77dbf1eeeb347135dc14f3aa80
“abhijeetardale” <abhijeet.ardale@yahoo.co.in>
2019-12-16 08:04:41 +0000

    more screnarios added to check the distance

--- test/service/LocationServiceSpec.scala
+++ test/service/LocationServiceSpec.scala
@@ -10,6 +10,9 @@ class LocationServiceSpec extends PlaySpec with GuiceOneAppPerSuite with MustMat
 
   val service = new LocationService()
   val userLondon = User(1, "Maurise", "Shieldon", "mshieldon0@squidoo.com", "192.57.232.111", 51.50853, -0.12574)
+  val userLondon25Mile = User(2, "Bendix", "Halgarth", "bhalgarth1@timesonline.co.u", "4.185.73.82", 51.803615, 0.137487)
+  val userLondon50Mile = User(3, "Meghan", "Southall", "msouthall2@ihg.com", "21.243.184.21", 51.17894000, -1.07006000)
+  val userLondonMoreThan50Mile = User(4, "Sidnee", "Silwood", "ssilwood3@gizmodo.com", "77.55.231.220", 52.192001, -2.220000)
 
   "LocationService" when{
 
@@ -17,6 +20,18 @@ class LocationServiceSpec extends PlaySpec with GuiceOneAppPerSuite with MustMat
 
      "return true if distance is 0 mile away from London" in {
        service.locate(userLondon) mustBe true
+     }
+
+     "return true if distance is 25 mile away from London" in {
+       service.locate(userLondon25Mile) mustBe true
+     }
+
+     "return true if distance is exact 50 miles away from London" in {
+       service.locate(userLondon50Mile) mustBe true
+     }
+
+     "return false if distance is more than 50 miles away from London" in {
+       service.locate(userLondonMoreThan50Mile) mustBe false
      }
 
     }
